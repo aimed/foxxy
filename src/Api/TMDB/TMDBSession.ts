82888e6193faa6aa6e07882b72928d87c3f76fc9
Maximilian Täschner <maximilian.taeschner@gmail.com>
2017-10-27 22:30:28 +0300

    feat(session): adds ability to add delegates to observe sessions

--- src/Api/TMDB/TMDBSession.ts
+++ src/Api/TMDB/TMDBSession.ts
@@ -1,17 +1,34 @@
 import { TMDBAccount } from './TMDBAccount';
 
+export interface SessionDelegate {
+    onSessionCreated(session: TMDBSession): void;
+    onSessionDestroyed(session: TMDBSession): void;
+}
+
 export class TMDBSession {
-    
+    private static _sessionDelegates: Set<SessionDelegate> = new Set<SessionDelegate>();
     public sessionId: string;
     public account?: TMDBAccount;
-    
-    // private _account?: TMDBAccount;
-    
+
+    public static registerDelegate(delegate: SessionDelegate) {
+        TMDBSession._sessionDelegates.add(delegate);
+    }
+
+    public static unregisterDelegate(delegate: SessionDelegate) {
+        TMDBSession._sessionDelegates.delete(delegate);
+    }
+
     public static fromJSON(data: any): TMDBSession {
         return new TMDBSession(data.session_id);
     }
 
     public constructor(sessionId: string) {
         this.sessionId = sessionId;
+        TMDBSession._sessionDelegates.forEach(d => d.onSessionCreated(this));
+    }
+
+    public destroy() {
+        TMDBSession._sessionDelegates.forEach(d => d.onSessionDestroyed(this));
+        this.sessionId = '';
     }
 }
