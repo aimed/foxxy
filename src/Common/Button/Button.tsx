17ba9de296332c286eabd5042d81f3fc39d7332d
Maximilian Täschner <maximilian.taeschner@gmail.com>
2017-10-30 11:22:04 +0300

    fix(button): makes sound optional

--- src/Common/Button/Button.tsx
+++ src/Common/Button/Button.tsx
@@ -3,21 +3,24 @@ import './Button.css';
 import * as React from 'react';
 
 // Sound provided by http://facebook.design/soundkit
-const sound = require('./sound.m4a');
+const soundFile = require('./sound.m4a');
 
 interface ButtonProps extends 
     React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
         plain?: boolean;
+        sound?: boolean;
 }
 
 export class Button extends React.Component<ButtonProps, {}> {
-    private static audio = new Audio(sound);
+    private static audio = new Audio(soundFile);
 
     public render() {
-        const { className, onClick, plain, ...props } = this.props;
+        const { className, onClick, plain, sound, ...props } = this.props;
         
         const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-            Button.audio.play();
+            if (sound) {
+                Button.audio.play();
+            }
             
             if (onClick) {
                 onClick(event);
