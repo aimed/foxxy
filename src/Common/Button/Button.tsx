80725d2ee53c5c96a3ecae64f27048d348119fa2
Maximilian Täschner <maximilian.taeschner@gmail.com>
2017-11-04 11:16:08 +0300

    feat(Button): adds material options to button

--- src/Common/Button/Button.tsx
+++ src/Common/Button/Button.tsx
@@ -10,14 +10,32 @@ const soundFile = require('./sound.m4a');
 interface ButtonProps extends 
     React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
         primary?: boolean;
+        secondary?: boolean;
         sound?: boolean;
+        raised?: boolean;
+        unelevated?: boolean;
+        stroked?: boolean;
+        dense?: boolean;
+        compact?: boolean;
 }
 
 export class Button extends React.Component<ButtonProps, {}> {
     private static audio = new Audio(soundFile);
 
     public render() {
-        const { className, onClick, primary, sound, ...props } = this.props;
+        const { 
+            className, 
+            onClick, 
+            primary, 
+            secondary,
+            raised,
+            unelevated,
+            stroked,
+            dense,
+            compact,
+            sound, 
+            ...props 
+        } = this.props;
         
         const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
             if (sound) {
@@ -32,7 +50,13 @@ export class Button extends React.Component<ButtonProps, {}> {
         const classNames = classnames([
             className,
             'mdc-button',
-            primary && 'mdc-theme--primary-bg mdc-theme--text-primary-on-primary'
+            primary && 'mdc-theme--primary-bg mdc-theme--text-primary-on-primary',
+            secondary && 'mdc-theme--secondary-bg mdc-theme--text-secondary-on-secondary',
+            raised && 'mdc-button--raised',
+            unelevated && 'mdc-button--unelevated',
+            stroked && 'mdc-button--stroked',
+            dense && 'mdc-button--dense',
+            compact && 'mdc-button--compact'
         ]);
         
         return (
