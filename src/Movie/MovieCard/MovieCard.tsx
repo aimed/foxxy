a9db4c2656676aba13146abcad547cefd8bef105
Maximilian Täschner <maximilian.taeschner@gmail.com>
2017-11-09 12:31:52 +0300

    feat(MovieCard): adds movie image

--- src/Movie/MovieCard/MovieCard.tsx
+++ src/Movie/MovieCard/MovieCard.tsx
@@ -3,6 +3,7 @@ import './MovieCard.css';
 import * as React from 'react';
 
 import { Card } from '../../Common/Card/Card';
+import { MoviePoster } from '../MoviePoster/MoviePoster';
 import { TMDBMovie } from '../../Api/TMDB/TMDBMovie';
 
 export interface MovieCardState { }
@@ -18,6 +19,7 @@ export class MovieCard extends React.Component<MovieCardProps, MovieCardState> {
                 className="movie-card"
                 title={movie.title}
                 subtitle={movie.releaseDate.getFullYear() + ''}
+                avatar={<MoviePoster movie={movie} />}
             />
         );
     }
