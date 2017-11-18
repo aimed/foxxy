e5258ee9e95fed8682cefc6e12c0b1411be6e6e1
Maximilian Täschner <maximilian.taeschner@gmail.com>
2017-11-18 14:44:03 +0100

    feat(MovieRatings): adds a movie rating component

---
+++ src/Movie/MovieRating/MovieRating.tsx
@@ -0,0 +1,26 @@
+import './MovieRating.css';
+
+import * as React from 'react';
+
+import { TMDBMovie } from '../../Api/TMDB/TMDBMovie';
+
+export interface MovieRatingState {}
+export interface MovieRatingProps {
+    movie: TMDBMovie;
+}
+
+export class MovieRating extends React.Component<MovieRatingProps, MovieRatingState> {
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <div className="movie-rating">
+        <div className="movie-rating__bar">
+          <span className="movie-rating__vote-average">{movie.voteAverage}</span>
+          <span className="movie-rating__vote-average-bar" style={{ right: `${(movie.voteAverage * 10)}%` }} />
+        </div>
+        <div className="movie-rating__vote-count">{movie.voteCount}</div>
+      </div>
+    );
+  }
+}
